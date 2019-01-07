88905fa33ae7f415ebeafb8d2317a8cbe9cc9afe
mpif <caiszf@163.com>
2019-01-07 19:01:07 +0800

    新增单元测试

---
+++ server-tomcat/src/test/java/com/codefans/concurrency/servercommon/service/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.codefans.concurrency.servercommon.service;
+
+import com.codefans.concurrency.servercommon.domain.UserDO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author:
+ * @date: 2019-01-07 18:29:37
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring-config.xml"})
+@TestPropertySource(properties={"env=dev"})
+public class UserServiceTest {
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void minusAmountTest() {
+
+        UserDO userDO = new UserDO();
+        userDO.setId(1L);
+        userDO.setAmount(1L);
+        int updateCount = userService.minusAmount(userDO);
+        System.out.println("updateCount=" + updateCount);
+
+
+    }
+
+}
